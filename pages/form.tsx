57d00277068f3dc03eca4c0214025bdb1e35f052
Kdy123 <123481645+Whited123@users.noreply.github.com>
2023-07-05 21:07:39 +0900

    리액트 훅 폼
    다 기억은 나는데 복습중 ㅋㅋ 에러 메시지 표시하기

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,28 +1,41 @@
 import { useForm } from "react-hook-form";
 
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onVaild = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onVaild = (data: LoginForm) => {
     console.log("하이 베이비");
+  };
+  const onInbvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
   return (
     <form onSubmit={handleSubmit(onVaild)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            message: "Your username is too short.",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="ID"
       />
       <input
         {...register("email", {
-          required: true,
+          required: "Password is required",
         })}
         placeholder="Email"
       />
       <input
         {...register("password", {
-          required: true,
+          required: "Email is required",
         })}
         type="password"
         placeholder="Password"
